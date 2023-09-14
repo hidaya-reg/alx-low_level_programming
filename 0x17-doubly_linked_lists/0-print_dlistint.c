3826c163858f40f87f7e3649c27c847efaf55229
hidaya <hidaya.regragui@gmail.com>
2023-09-14 01:59:26 -0700

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t n = 0;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		n++;
+	}
+	return (n);
+}
