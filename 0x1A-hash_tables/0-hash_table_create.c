e8eb59e135577dcecb10ccc5fd0fccacce3f5505
hidaya <hidaya.regragui@gmail.com>
2023-10-05 15:13:08 -0700

    task1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -24,7 +24,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table->array == NULL)
 	{
 		free(new_table);
-		return(NULL);
+		return (NULL);
 	}
 
 	/* Initialize the array elts to NULL */
