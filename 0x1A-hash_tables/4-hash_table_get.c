dfcdda1bb9b045756a311d080a515cad4956fd21
hidaya <hidaya.regragui@gmail.com>
2023-10-06 02:00:17 -0700

    task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash to look into
+ * @key: key we are lloking for
+ * Return: velue associated with key, NULL if key don't exist
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+	return (NULL);
+}
