5cc9099e38305916f4aa5a963827c7fe71102b52
hidaya <hidaya.regragui@gmail.com>
2024-02-07 20:36:11 +0100

    task 9

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,14 +2,11 @@
 #define SEARCH_ALGOS_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Function prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-int binary_search_recursive(int *array, size_t left, size_t right, int value);
 
 #endif
