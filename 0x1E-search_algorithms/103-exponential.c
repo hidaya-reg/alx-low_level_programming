498a6f67b6443fe61c97853d44e5eecde53ee257
hidaya <hidaya.regragui@gmail.com>
2024-03-13 22:00:06 +0000

    task 10

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,73 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * exponential_search - performs exponential search
+ * @array: the integer array
+ * @size: its size
+ * @value: value to search for
+ *
+ * Return: the index found or -1
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t i = 1, newsize = 0;
+	int ret;
+
+	if (!array || size == 0)
+		return (-1);
+
+	while (i < size && array[i] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		i <<= 1;
+	}
+
+	newsize = (i >= size ? size : i + 1) - (i >> 1);
+	i >>= 1;
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			i, i << 1 >= size ? size - 1 : i << 1);
+	ret = binary_search(array + i, newsize, value);
+	return (ret == -1 ? ret : ret + (int)i);
+}
+
+/**
+ * binary_search - performs binary search
+ * @array: the integer array
+ * @size: its size
+ * @value: value to search for
+ *
+ * Return: the index found or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	int *a = array;
+
+	if (!array)
+		return (-1);
+
+	while (size)
+	{
+		printf("Searching in array: ");
+		for (i = 0; i < size; i++)
+		{
+			printf("%d", a[i]);
+			if (i + 1 != size)
+				printf(", ");
+			else
+				printf("\n");
+		}
+		i = (size - 1) / 2;
+		if (a[i] == value)
+			return ((a - array) + i);
+		else if (a[i] > value)
+			size = i;
+		else
+		{
+			a += (i + 1);
+			size -= (i + 1);
+		}
+	}
+	return (-1);
+}
